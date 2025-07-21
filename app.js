53468679714ab6bf977f6c640f3b2d4fcc702dc9
Pratap Prajapati <144755984+pratapprajapati998@users.noreply.github.com>
2025-07-21 19:32:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const btn = document.getElementById('myButton');
+
+btn.addEventListener("click", function () {
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+    console.log("updated color");
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
